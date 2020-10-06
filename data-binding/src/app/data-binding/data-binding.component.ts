69c046a6119fe04ebe0da4463263b4d2546b1605
Ivan Vidal <ivanvalmeida.123@gmail.com>
2020-10-05 23:14:57 -0300

    event-add

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -8,15 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class DataBindingComponent implements OnInit {
   urlImage= 'http://lorempixel.com/400/200/sports/';
   url = "https://ivan-vidal.com"
-CurtirCurso = true
+
+  CurtirCurso = true
+
+  AtualValue = ''
+  valorSalvo = ''
+  IsMouseOver: boolean = false
+
   getValor(){
     return 4
   }
   CursAngular(){
     return true
   }
+  OnmouseOverOut(){
+this.IsMouseOver = !this.IsMouseOver
+  }
 
+botaoClicado(){
+  alert("Olá")
+}
+onKeyUp(evento:KeyboardEvent){
+this.AtualValue =(<HTMLInputElement>evento.target).value
+}
+salvarValor(valor){
 
+  this.valorSalvo = valor
+}
 
   constructor() { }
 
