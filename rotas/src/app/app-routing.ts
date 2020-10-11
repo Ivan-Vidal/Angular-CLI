58bff039f3ef8b3689a6b8d1ed3d7ac596b4657a
Ivan Vidal <ivanvalmeida.123@gmail.com>
2020-10-10 23:55:46 -0300

    create rotas

--- rotas/src/app/app-routing.ts
+++ rotas/src/app/app-routing.ts
@@ -6,9 +6,9 @@ import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
-{path:'cursos',component: CursosComponent },
-{path:'login',component: LoginComponent },
-{path:'',component: HomeComponent }
+{path:'cursos', component: CursosComponent },
+{path:'login', component: LoginComponent },
+{path:'', component: HomeComponent }
 
 ];
 
